47990ff1795cd3a7ef1bcf2b3567a0f12fa66b2b
Sergii Rudenko <rudenko.sergey92@gmail.com>
2020-01-25 12:15:11 -0800

    Images

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,39 +1,46 @@
+import { useState } from 'react';
 import { Header } from './components/header';
 import { Footer } from './components/footer';
 import Link from 'next/link';
 
+const defaultImg = 'img/Index/Material Test 002.png';
+
 function HomePage() {
+  const [heroImg, setHeroImg] = useState(defaultImg);
+  const setDefaultImage = () => setHeroImg(defaultImg);
   return (
     <div>
       <Header isMain />
       <div className="index-bottom wrapper">
         <div className="casestudies">
           <ul>
-            <li className="menu-link">
-              <Link
-                href="/001-Smarter-Keyboard"
-                data-url="img/001-Design-Keyboard/001-Intro.png"
-              >
-                Smart Mobile Keyboard
-              </Link>
+            <li
+              className="menu-link"
+              onMouseEnter={() =>
+                setHeroImg('img/001-Design-Keyboard/001-Intro.png')
+              }
+              onMouseLeave={setDefaultImage}
+            >
+              <Link href="/001-Smarter-Keyboard">Smart Mobile Keyboard</Link>
             </li>
-            <li className="menu-link">
-              <Link href="/003-Style" data-url="img/003-Style/001-Intro.png">
-                Writing Style Guide
-              </Link>
+            <li
+              className="menu-link"
+              onMouseEnter={() => setHeroImg('img/003-Style/001-Intro.png')}
+              onMouseLeave={setDefaultImage}
+            >
+              <Link href="/003-Style">Writing Style Guide</Link>
             </li>
-            <li className="menu-link">
-              <Link
-                href="/002-Marketing"
-                data-url="img/002-Marketing/001-Intro.png"
-              >
-                Grammarly Marketing
-              </Link>
+            <li
+              className="menu-link"
+              onMouseEnter={() => setHeroImg('img/002-Marketing/001-Intro.png')}
+              onMouseLeave={setDefaultImage}
+            >
+              <Link href="/002-Marketing">Grammarly Marketing</Link>
             </li>
           </ul>
         </div>
         <div className="demo">
-          <img className="hero-img" src="img/Index/Material Test 002.png" />
+          <img className="hero-img" src={heroImg} />
         </div>
       </div>
 
