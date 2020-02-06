ceeb61ffc978aa9b18cd03c32aad72c8f18bae85
Yura Tan <byorickq@gmail.com>
2020-02-05 17:11:42 -0800

    3d switcher init

--- js/main.js
+++ js/main.js
@@ -1,49 +1,30 @@
-function init() {
-  const width = parseFloat(
-    getComputedStyle(document.documentElement, null).width.replace("px", "")
-  );
-  const materialImg = "img/Index/Material Test 002.png";
-
-  if (width > 800) {
-    const menuLink = document.querySelectorAll(".menu-link");
-    const heroImg = document.querySelector(".hero-img");
-    const hideOnScroll = document.querySelector(".hideOnScroll");
-    const shrinkOnScroll = document.querySelector(".shrinkOnScroll");
-    menuLink.forEach(el => {
-      el.addEventListener("mouseenter", function(link) {
-        heroImg.setAttribute("src", link.target.dataset.url);
-      });
-      el.addEventListener("mouseleave", function(link) {
-        heroImg.setAttribute("src", materialImg);
-      });
-    });
+$(document).ready(function(){
+  
+  if ($(window).width() > 800) {
+    
+    $(".casestudies ul li a").hover(
+      function(){
+        $(".demo img").attr("src", $(this).data('url'));
+      },
+      function(){
+        $(".demo img").attr("src", "");
+    })
 
     checkScroll();
-    window.addEventListener("scroll", function() {
-      checkScroll();
+
+    $(window).scroll(function(){
+       checkScroll();
     });
 
-    function checkScroll() {
-      const scrollPos = document.documentElement.scrollTop;
-      if (scrollPos > 0) {
-        hideOnScroll.classList.add("hide");
-        hideOnScroll.classList.remove("show");
-        shrinkOnScroll.classList.add("animate");
-      } else {
-        hideOnScroll.classList.add("show");
-        hideOnScroll.classList.remove("remove");
-        shrinkOnScroll.classList.remove("animate");
-      }
+    function checkScroll(){
+       var scrollPos = $(document).scrollTop();
+       if (scrollPos > 0) {
+          $(".hideOnScroll").fadeOut(200);
+          $(".shrinkOnScroll").addClass("animate");
+        } else {
+          $(".hideOnScroll").fadeIn(200);
+          $(".shrinkOnScroll").removeClass("animate");
+        }
     }
   }
-}
-
-function ready() {
-  if (document.readyState != "loading") {
-    init();
-  } else {
-    document.addEventListener("DOMContentLoaded", init);
-  }
-}
-
-ready();
+});
