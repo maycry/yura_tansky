51f55cb3a4771e8ec2ed21e61232bcab62653821
maycry <byorickq@gmail.com>
2020-12-20 18:51:55 -0800

    favicon

--- js/main.js
+++ js/main.js
@@ -1,14 +1,6 @@
 $(document).ready(function(){
   
   if ($(window).width() > 800) {
-    
-    // $(".casestudies ul li a").hover(
-    //   function(){
-    //     $(".demo img").attr("src", $(this).data('url'));
-    //   },
-    //   function(){
-    //     $(".demo img").attr("src", "img/Index/Material Test 002.png");
-    // })
 
     checkScroll();
 
@@ -17,14 +9,14 @@ $(document).ready(function(){
     });
 
     function checkScroll(){
-       var scrollPos = $(document).scrollTop();
-       if (scrollPos > 0) {
-          $(".hideOnScroll").fadeOut(200);
-          $(".shrinkOnScroll").addClass("animate");
-        } else {
-          $(".hideOnScroll").fadeIn(200);
-          $(".shrinkOnScroll").removeClass("animate");
-        }
+      var scrollPos = $(document).scrollTop();
+      if (scrollPos > 0) {
+        $(".hideOnScroll").fadeOut(200);
+        $(".shrinkOnScroll").addClass("animate");
+      } else {
+        $(".hideOnScroll").fadeIn(200);
+        $(".shrinkOnScroll").removeClass("animate");
+      }
     }
   }
 });
